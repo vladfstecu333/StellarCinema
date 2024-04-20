752814253d4e5b7699274d45387507fbbd69a1f8
imtouchk <imtouchk@gmail.com>
2024-04-20 10:26:40 +0300

    Bootstrap

--- client/Stellar-Cinema/src/App.jsx
+++ client/Stellar-Cinema/src/App.jsx
@@ -1,33 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import "bootstrap/dist/js/bootstrap.bundle"
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="dropdown">
+        <button className="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Dropdown
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <ul className="dropdown-menu">
+          <li><button className="dropdown-item" type="button">Dropdown item</button></li>
+          <li><button className="dropdown-item" type="button">Dropdown item</button></li>
+          <li><button className="dropdown-item" type="button">Dropdown item</button></li>
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
