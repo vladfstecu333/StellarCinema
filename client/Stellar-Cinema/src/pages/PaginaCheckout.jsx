4d2dcd9d4f42be2518e13dc9a601e2f2afc9d855
vladsfetcu333 <vladissimo123@gmail.com>
2024-04-20 20:54:55 +0300

    Smecherie

--- client/Stellar-Cinema/src/pages/PaginaCheckout.jsx
+++ client/Stellar-Cinema/src/pages/PaginaCheckout.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './PaginaCheckout.css'
+import BookOfRa from './BookOfRa'
 
 const PaginaCheckout = () => {
-    return(
-        <div class='introducere'>
-    <h1 id="final">Comanda ta a fost inregistrata cu succes! Multumim!</h1>
+    return(<>
+    <div class='introducere' id="int">
+        <h1 id="final">Comanda ta a fost inregistrata cu succes! Multumim!</h1>
     </div>
+    <BookOfRa />
+    </>
     )
 }
 
