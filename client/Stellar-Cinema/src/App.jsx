8f0430167d5256e1d57692624d108fd88e6fcf40
vladsfetcu333 <vladissimo123@gmail.com>
2024-04-20 18:22:57 +0300

    Zi ma

--- client/Stellar-Cinema/src/App.jsx
+++ client/Stellar-Cinema/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<IntroPage />} />
         <Route path='events' element={<Evenimente />} />
+        <Route path='book' element={<Rezervare />} />
       </Routes>
     </Router>
     </>
